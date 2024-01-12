39450516017bc7548b51e03f197484c4ccf976a9
BohdanVlas <bodik.vlasenko@gmail.com>
2024-01-12 15:21:25 +0200

    tf changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">=0.13.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.32.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
